4894896e176bb0b8ce452f9c41af1e471da99cbb
Luca Krohm <luc_kro@uni-bremen.de>
2025-01-04 02:33:38 +0100

    [helper] added round pose and fixed some stuff

--- src/pycram/helper.py
+++ src/pycram/helper.py
@@ -10,7 +10,7 @@ from pycram.robot_description import RobotDescription, GraspDescription
 from scipy.spatial.transform import Rotation as R
 from typing_extensions import List, Union, Optional
 
-from pycram.ros.viz_marker_publisher import AxisMarkerPublisher
+from pycram.ros.viz_marker_publisher import AxisMarkerPublisher, ManualMarkerPublisher, ArrowArrayPublisher
 from pycram.world_concepts.world_object import Object
 
 """Implementation of helper functions and classes for internal usage only.
@@ -57,45 +57,46 @@ FACE_TO_AXIS_INDEX = {
 }
 
 
-def calculate_vector_face(vector: List, side_axis: Optional[AxisIdentifier] = None) -> List[Grasp]:
+def calculate_object_faces(object_to_robot_vector: List, specified_grasp_axis: Optional[AxisIdentifier] = None) -> List[
+    Grasp]:
     """
     Determines the faces of the object based on the input vector.
 
-    If `side_axis` is None, it calculates the primary and secondary faces based on the vector's magnitude
+    If `specified_grasp_axis` is None, it calculates the primary and secondary faces based on the vector's magnitude
     in the x and y directions, determining which sides of the object are most aligned with the robot.
-    If `side_axis` is provided, it only considers the specified axis and calculates the faces aligned
+    If `specified_grasp_axis` is provided, it only considers the specified axis and calculates the faces aligned
     with that axis.
 
     Args:
-        vector (List): A 3D vector representing one of the robot's axes in the object's frame, with
+        object_to_robot_vector (List): A 3D vector representing one of the robot's axes in the object's frame, with
                        irrelevant components set to np.nan.
-        side_axis (Optional[AxisIdentifier]): Specifies a specific axis (e.g., X, Y, Z) to focus on.
+        specified_grasp_axis (Optional[AxisIdentifier]): Specifies a specific axis (e.g., X, Y, Z) to focus on.
 
     Returns:
         List[Grasp]: A list of two Grasp enums representing the primary and secondary faces.
     """
     epsilon = 1e-15
 
-    vector = np.where(np.isnan(vector), np.nan, vector + epsilon)
+    object_to_robot_vector = np.where(np.isnan(object_to_robot_vector), np.nan, object_to_robot_vector + epsilon)
 
-    if side_axis is None:
+    if specified_grasp_axis is None:
         valid_indices = [AXIS_TO_INDEX['x'], AXIS_TO_INDEX['y'], AXIS_TO_INDEX['z']]
     else:
-        valid_indices = [AXIS_TO_INDEX[side_axis.name.lower()]]
+        valid_indices = [AXIS_TO_INDEX[specified_grasp_axis.name.lower()]]
 
-    valid_indices = [i for i in valid_indices if not np.isnan(vector[i])]
+    valid_indices = [i for i in valid_indices if not np.isnan(object_to_robot_vector[i])]
 
-    abs_vector = np.abs(vector)
+    abs_vector = np.abs(object_to_robot_vector)
     sorted_indices = sorted(valid_indices, key=lambda i: abs_vector[i], reverse=True)
 
     primary_index = sorted_indices[0]
-    primary_sign = int(np.sign(vector[primary_index]))
+    primary_sign = int(np.sign(object_to_robot_vector[primary_index]))
     primary_axis = INDEX_TO_AXIS[primary_index]
     primary_face = AXIS_INDEX_TO_FACE[(primary_axis, primary_sign)]
 
     if len(sorted_indices) > 1:
         secondary_index = sorted_indices[1]
-        secondary_sign = int(np.sign(vector[secondary_index]))
+        secondary_sign = int(np.sign(object_to_robot_vector[secondary_index]))
         secondary_axis = INDEX_TO_AXIS[secondary_index]
         secondary_face = AXIS_INDEX_TO_FACE[(secondary_axis, secondary_sign)]
     else:
@@ -106,7 +107,8 @@ def calculate_vector_face(vector: List, side_axis: Optional[AxisIdentifier] = No
     return [primary_face, secondary_face]
 
 
-def calculate_grasp_configs(target_object: ObjectDesignatorDescription.Object, robot: Optional[Object] = None) -> List[GraspDescription]:
+def calculate_grasp_descriptions(target_object: ObjectDesignatorDescription.Object, robot: Optional[Object] = None) -> \
+List[GraspDescription]:
     """
     Calculates the grasp configurations of an object relative to the robot based on orientation and position.
 
@@ -121,7 +123,8 @@ def calculate_grasp_configs(target_object: ObjectDesignatorDescription.Object, r
         List[GraspConfig]: A sorted list of GraspConfig instances representing all grasp permutations.
     """
 
-    obj_desig = target_object if isinstance(target_object, (ObjectDesignatorDescription.Object, Pose)) else target_object.resolve()
+    obj_desig = target_object if isinstance(target_object,
+                                            (ObjectDesignatorDescription.Object, Pose)) else target_object.resolve()
 
     oTm = obj_desig.pose
     base_link = RobotDescription.current_robot_description.base_link
@@ -145,10 +148,10 @@ def calculate_grasp_configs(target_object: ObjectDesignatorDescription.Object, r
     vector_x, vector_y, vector_z = vector_to_robot_local
 
     vector_facing = np.array([vector_x, vector_y, np.nan], dtype=float)
-    side_faces = calculate_vector_face(vector_facing, side_axis)
+    side_faces = calculate_object_faces(vector_facing, side_axis)
 
     vector_z = np.array([np.nan, np.nan, vector_z], dtype=float)
-    top_faces = calculate_vector_face(vector_z) if top else [None]
+    top_faces = calculate_object_faces(vector_z) if top else [None]
 
     grasp_configs = [
         GraspDescription(side_face=side, top_face=top_face, horizontal=horizontal)
@@ -156,8 +159,51 @@ def calculate_grasp_configs(target_object: ObjectDesignatorDescription.Object, r
         for side in side_faces
     ]
 
-    return grasp_configs
+    if obj_desig.world_object.world.current_world.allow_publish_debug_poses:
+        # Assign colors based on side grasp and publish poses
+        marker = ArrowArrayPublisher()
+        poses = []
+        colors = []
+        labels = []
+        label_poses = []
 
+        color_map = {
+            Grasp.FRONT: [0.0, 1.0, 0.0, 1.0],  # Green
+            Grasp.LEFT: [0.0, 0.0, 1.0, 1.0],  # Blue
+            Grasp.RIGHT: [1.0, 1.0, 0.0, 1.0],  # Yellow
+            Grasp.BACK: [1.0, 0.0, 0.0, 1.0]  # Red
+        }
+
+        for idx, grasp_config in enumerate(grasp_configs):
+            oTm_copy = oTm.copy()
+            side_grasp, top_grasp, horizontal = grasp_config.side_face, grasp_config.top_face, grasp_config.horizontal
+            end_effector = RobotDescription.current_robot_description.get_arm_chain(Arms.LEFT).end_effector
+            grasp_descriptions: dict = end_effector.calculate_grasp_descriptions([0, 0, 0, 1])
+            grasp_orientation = grasp_descriptions.get((side_grasp, top_grasp, horizontal))
+            palm_axis = [1, 0, 0]
+
+            if obj_desig.obj_type == ObjectType.BOWL:
+                rim_offset = calculate_rim_grasp(obj_desig.world_object.get_object_dimensions(), side_grasp)
+                rim_direction = grasp_descriptions.get((side_grasp, None, False))
+                rim_adjustment = adjust_grasp_for_object_rotation(oTm_copy, rim_direction)
+                rim_pose = translate_relative_to_object(rim_adjustment, palm_axis, rim_offset)
+                oTm_copy.position = rim_pose.position
+
+            adjusted_oTm = adjust_grasp_for_object_rotation(oTm_copy, grasp_orientation)
+            translation_value = max(obj_desig.world_object.get_object_dimensions()) * 0.75
+            pose = translate_relative_to_object(adjusted_oTm, palm_axis, translation_value)
+            color = color_map.get(side_grasp, None)
+            colors.append(color)
+            poses.append(pose)
+
+            labels.append(f"Try {idx + 1}")
+            label_pose = pose.copy()
+            label_pose = translate_relative_to_object(label_pose, palm_axis, 0.05)
+            label_poses.append(label_pose)
+
+        marker.publish(poses, colors, labels, label_poses)
+
+    return grasp_configs
 
 
 def calculate_grasp_offset(object_dim: List, arm, grasp):
@@ -231,10 +277,11 @@ def get_preferred_grasp_alignment(object: Object) -> (bool, bool):
     Returns:
         tuple: A tuple of two booleans, indicating whether the object should be grasped horizontally and/or from the top
     """
-    object_type = object.obj_type if isinstance(object, ObjectDesignatorDescription.Object) else ObjectType.GENERIC_OBJECT
+    object_type = object.obj_type if isinstance(object,
+                                                ObjectDesignatorDescription.Object) else ObjectType.GENERIC_OBJECT
     preferred_alignment_dict = {ObjectType.BOWL: (None, True, True),
                                 ObjectType.SPOON: (AxisIdentifier.X, False, True),
-                                ObjectType.BREAKFAST_CEREAL: (AxisIdentifier.X, False, False),}
+                                ObjectType.BREAKFAST_CEREAL: (AxisIdentifier.X, False, False), }
 
     sidegrasp_axis, grasp_horizontal, grasp_top = preferred_alignment_dict.get(object_type, (None, False, False))
 
@@ -261,12 +308,34 @@ def translate_relative_to_object(obj_pose, palm_axis, translation_value) -> Pose
 
     quat = object_pose.orientation_as_list()
 
-    rotation_matrix = R.from_quat(quat).as_matrix()
-
-    retraction_world = rotation_matrix @ local_retraction
+    rotation_matrix = R.from_quat(quat)
+    retraction_world = rotation_matrix.apply(local_retraction)
 
     object_pose.pose.position.x -= retraction_world[0]
     object_pose.pose.position.y -= retraction_world[1]
     object_pose.pose.position.z -= retraction_world[2]
 
     return object_pose
+
+
+def round_pose(pose: Pose, decimals: int = 8) -> Pose:
+    """
+    Rounds the position and orientation of a pose to the specified number of decimal places.
+
+    Args:
+        pose (Pose): The pose to be rounded.
+        decimals (int): The number of decimal places to round to.
+
+    Returns:
+        Pose: The rounded pose.
+    """
+    rounded_pose = Pose(frame=pose.frame,
+                        position=[round(pose.position.x, decimals),
+                                  round(pose.position.y, decimals),
+                                  round(pose.position.z, decimals)],
+                        orientation=[round(pose.orientation.x, decimals),
+                                     round(pose.orientation.y, decimals),
+                                     round(pose.orientation.z, decimals),
+                                     round(pose.orientation.w, decimals)])
+
+    return rounded_pose
