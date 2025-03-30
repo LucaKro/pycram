43cc5d437820e3662aec09f0b4528c643583f7cd
Luca Krohm <luc_kro@uni-bremen.de>
2025-03-30 19:10:04 +0200

    [ci_test] testing if tests still run

--- src/pycram/ros_utils/viz_marker_publisher.py
+++ src/pycram/ros_utils/viz_marker_publisher.py
@@ -72,14 +72,12 @@ class VizMarkerPublisher:
                 continue
             for link in obj.link_name_to_id.keys():
                 geoms = obj.get_link_geometry(link)
-                print("Debug info test", flush=True)
-                logging.error("Debug info test2")
                 try:
                     length = len(geoms)
                     geom = geoms[0] if length > 0 else None
                 except Exception as e:
                     logerr(f"geoms: {geoms}, Exception: {e}")
-                    print(f"geoms: {geoms}, Exception: {e}")
+                    print(f"geoms: {geoms}, Exception: {e}", flush=True)
                     continue
                 if not geom:
                     continue
